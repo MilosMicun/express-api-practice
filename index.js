888a65bf9195de897d7d3af0d88c380c816dacd0
Milos Mirkovic <milosdmirkovic@gmai.com>
2026-01-05 13:51:50 +0100

    feat: add global logging middleware

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
